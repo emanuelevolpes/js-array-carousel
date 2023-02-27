b669df81d3cee740c7ea1170fcd3c82f78f1c22a
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-02-27 18:33:57 +0100

    last commit milestones

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const images = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
-
+const carousel = document.querySelector('.carousel');
 let imagesContent = '';
 let active = 0;
-
-const carousel = document.querySelector('.carousel');
 
 for (let i = 0; i < images.length; i++) {
     console.log(images[i]);
@@ -17,12 +15,10 @@ document.querySelector('.image').classList.add('show');
 
 const up = document.querySelector('.up');
 const down = document.querySelector('.down');
-
 const image = document.querySelectorAll('.image');
 
 up.addEventListener('click', function() {
     if (active > 0) {
-        console.log('decremento active');
         image[active].classList.remove('show');
         active--;
         image[active].classList.add('show');
@@ -31,7 +27,6 @@ up.addEventListener('click', function() {
 
 down.addEventListener('click', function() {
     if (active < image.length - 1) {
-        console.log('incremento active');
         image[active].classList.remove('show');
         active++;
         image[active].classList.add('show');
